36c82eb30506d79e6c623074e2da1c10a3881ce8
Pierre Balaye <pierre@balaye.fr>
2023-05-31 11:49:59 +0000

    Fix barycentre function

--- R/barycentre.R
+++ R/barycentre.R
@@ -1,9 +1,18 @@
+#' Title
+#'
+#' @param sf_tbl
+#' @param weights
+#'
+#' @return
+#' @export
+#'
+#' @examples
 barycentre <- function(sf_tbl, weights) {
  ((
   sf_tbl
   %>% dplyr::mutate(w = weights)
- %>% mutate(lon = map_dbl(cities$geometry, function(g) g[1]),
-            lat = map_dbl(cities$geometry, function(g) g[2]),
+ %>% mutate(lon = map_dbl(sf_tbl$geometry, function(g) g[1]),
+            lat = map_dbl(sf_tbl$geometry, function(g) g[2]),
             w.lon = w * lon,
             w.lat = w * lat)
  %>% summarise(across(all_of(c("w.lon", "w.lat", "w")), sum))
@@ -13,4 +22,3 @@ barycentre <- function(sf_tbl, weights) {
  st_sfc(st_point(c(centroid$c.lon, centroid$c.lat)))
 }
 
-d
